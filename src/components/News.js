ce3d0b033a121a66f73ecf7c2c839a94dd3b84e2
unknown <somyagang@gmail.com>
2023-04-22 13:16:23 +0530

    update

--- src/components/News.js
+++ src/components/News.js
@@ -11,11 +11,43 @@ export default class News extends Component {
     }
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ce1f5de0a39849bb991b5a103008fcda";
+  handleIndianNews = async() => {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=news";
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(data);
+    this.setState({articles: parsedData.articles})
+  }
+
+  handleTeslaNews = async() => {
+    let url = "https://newsapi.org/v2/everything?q=tesla&from=2023-03-22&sortBy=publishedAt&apiKey=ce1f5de0a39849bb991b5a103008fcda";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(data);
+    this.setState({articles: parsedData.articles})
+  }
+
+  handleCricketNews = async() => {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=cri";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(data);
+    this.setState({articles: parsedData.articles})
+  }
+
+  handleNDTVNews = async() => {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=ndtv";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(data);
+    this.setState({articles: parsedData.articles})
+  }
+  
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=news";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
     this.setState({articles: parsedData.articles})
   }
 
@@ -23,6 +55,10 @@ export default class News extends Component {
     return (
       <div className="container my-3">
         <h2 className="mt-5 mb-3">Soga News - Trending News World Wide!</h2>
+        <button type="button" className="btn btn-outline-info btn-lg" onClick={this.handleIndianNews}>Indian News</button>
+        <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleTeslaNews}>Tesla News</button>
+        <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleCricketNews}>Cricket News</button>
+        <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleNDTVNews}>NDTV News</button>
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-3" key={element.url}>
