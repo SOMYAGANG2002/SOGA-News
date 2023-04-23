92ca8166d00660abea766ad03d6c90b893fdf2da
unknown <somyagang@gmail.com>
2023-04-23 22:43:23 +0530

    update

--- src/components/News.js
+++ src/components/News.js
@@ -19,16 +19,24 @@ export default class News extends Component {
     this.setState({articles: parsedData.articles})
   }
 
-  handleTeslaNews = async() => {
-    let url = "https://newsapi.org/v2/everything?q=tesla&from=2023-03-22&sortBy=publishedAt&apiKey=ce1f5de0a39849bb991b5a103008fcda";
+  handlePoliticsNews = async() => {
+    let url = "https://newsapi.org/v2/top-headlines?country=&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=politics";
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(data);
     this.setState({articles: parsedData.articles})
   }
 
-  handleCricketNews = async() => {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=cri";
+  handleTeslaNews = async() => {
+    let url = "https://newsapi.org/v2/everything?q=tesla&from=2023-04-22&sortBy=publishedAt&apiKey=ce1f5de0a39849bb991b5a103008fcda";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(data);
+    this.setState({articles: parsedData.articles})
+  }
+
+  handleSportsNews = async() => {
+    let url = "https://newsapi.org/v2/top-headlines?country=&apiKey=ce1f5de0a39849bb991b5a103008fcda&q=sport";
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(data);
@@ -56,9 +64,11 @@ export default class News extends Component {
       <div className="container my-3">
         <h2 className="mt-5 mb-3">Soga News - Trending News World Wide!</h2>
         <button type="button" className="btn btn-outline-info btn-lg" onClick={this.handleIndianNews}>Indian News</button>
+        <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handlePoliticsNews}>Politics</button>
         <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleTeslaNews}>Tesla News</button>
-        <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleCricketNews}>Cricket News</button>
+        <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleSportsNews}>Sports News</button>
         <button type="button" className="btn btn-outline-info btn-lg ms-3" onClick={this.handleNDTVNews}>NDTV News</button>
+
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-3" key={element.url}>
